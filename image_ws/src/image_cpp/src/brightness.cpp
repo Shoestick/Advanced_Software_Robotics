8182ff8dbaaeecb362537bdf79c916b049200401
Shoestick <oisindeb@gmail.com>
2025-02-20 10:10:42 +0100

    Now publishing whether above or below threshold to topic call /bright.
    Only showing when I use ros2 topic echo though, doesn't show up on rqt_grpah for some reason.

--- image_ws/src/image_cpp/src/brightness.cpp
+++ image_ws/src/image_cpp/src/brightness.cpp
@@ -1,20 +1,34 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
 #include "example_interfaces/msg/string.hpp"
+
+using namespace std::chrono_literals;
      
 class Brightness : public rclcpp::Node 
 {
 public:
     Brightness() : Node("brightness") 
     {
-        
         subscriber_ = this->create_subscription<sensor_msgs::msg::Image>(
         "image", 10,
         std::bind(&Brightness::callbackImage, this, std::placeholders::_1));
-        RCLCPP_INFO(this->get_logger(), "Brightness sub has begun");
+        
+        publisher_ = this->create_publisher<example_interfaces::msg::String>("bright", 10);
+        timer_ = this->create_wall_timer(0.5s, std::bind(&Brightness::publishBright, this));
+        
+        RCLCPP_INFO(this->get_logger(), "Brightness node has begun");
     }
  
 private:
+    void publishBright()
+    {
+        std::string condition {"above"};
+        if(bright_ == 0) condition = "below";
+
+        auto msg = example_interfaces::msg::String();
+        msg.data = std::string("Compared to the threshold, avg is ") + condition;
+        publisher_->publish(msg);
+    }
 
     void callbackImage(const sensor_msgs::msg::Image::SharedPtr msg)
     {
@@ -30,19 +44,17 @@ private:
         // so I'm judging this static cast as okay
         int avg { total / static_cast<int>(rows * step) };
 
-        const int threshold = 200;
+        const int threshold = 130;
         if(avg > threshold) bright_ = 1;
         else bright_ = 0;
 
-        const char* condition {"above"};
-        if(bright_ == 0) condition = "below";
-
-        RCLCPP_INFO(this->get_logger(), "compared to threshold, avg is '%s'", condition);
+        RCLCPP_INFO(this->get_logger(), "avg is currently '%d'", avg);
     }
 
     bool bright_ {0};
     rclcpp::Publisher<example_interfaces::msg::String>::SharedPtr publisher_;
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscriber_;
+    rclcpp::TimerBase::SharedPtr timer_;
 };
  
 int main(int argc, char **argv)
