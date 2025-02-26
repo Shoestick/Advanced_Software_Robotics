a87a8962092f1f80f8008d4c5d4b7b02199e87f7
Shoestick <oisindeb@gmail.com>
2025-02-26 15:55:14 +0100

    Tracking centre of mass of white spot, posting x and y coord to terminal

--- image_ws/src/image_cpp/src/obj_pos.cpp
+++ image_ws/src/image_cpp/src/obj_pos.cpp
@@ -1,6 +1,4 @@
-// grayscale or pick a colour channel
-// threshold into binary image
-// find the "centre of gravity" of the white pixels
+// find the "centre of gravity" of the white pixels, publish this to the terminal
 // publish this CoG to a topic
 
 #include "rclcpp/rclcpp.hpp"
@@ -13,12 +11,15 @@ class ObjectPosNode : public rclcpp::Node
 public:
     ObjectPosNode() : Node("obj_pos")
     {
+        // declare parameter controlling threshold value
         this->declare_parameter("threshold", 130);
 
+        // subscribe to /image topic and call callbackImage function when recieving data
         subscriber_ = this->create_subscription<sensor_msgs::msg::Image>(
         "image", 10,
         std::bind(&ObjectPosNode::callbackImage, this, std::placeholders::_1));
 
+        // text to terminal when starting
         RCLCPP_INFO(this->get_logger(), "Object position node has begun");
     }
 
@@ -26,19 +27,28 @@ private:
     void callbackImage(const sensor_msgs::msg::Image::SharedPtr msg)
     {
         // will need to change the "bgr8" to manage other formats of image
-        cv::Mat image = cv_bridge::toCvCopy(msg, "bgr8")->image;
-        cv::Mat grey {};
-        cvtColor(image, grey, CV_BGR2GRAY);
+        cv::Mat src = cv_bridge::toCvCopy(msg, "bgr8")->image;
 
-        cv::Mat dst {};
+        cv::Mat grey {};
+        cvtColor(src, grey, CV_BGR2GRAY);
+
+        cv::Mat threshed {};
 
         int const threshold_type {0};
         threshold_value_ = this->get_parameter("threshold").as_int();
 
-        threshold( grey, dst, threshold_value_, 255, threshold_type );
+        threshold( grey, threshed, threshold_value_, 255, threshold_type );
 
-        cv::imshow("OpenCV window", dst);
-        cv::waitKey('q');       
+        // find moments of the image
+        cv::Moments m = cv::moments(threshed, true);
+        cv::Point p(m.m10/m.m00, m.m01/m.m00);
+
+        RCLCPP_INFO(this->get_logger(), "Centroid of object: (%d, %d)", p.x, p.y);
+        
+        // show the image with a point mark at the centroid
+        circle(src, p, 5, cv::Scalar(128,0,0), -1);
+        cv::imshow("Image with center",src);
+        cv::waitKey('q'); 
     }
 
     int threshold_value_ {};
