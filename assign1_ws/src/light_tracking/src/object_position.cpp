e00a2593d4681dc6de5237ec70d46e5646a90aa1
Shoestick <oisindeb@gmail.com>
2025-02-28 11:37:50 +0100

    Object_position publishes (-1, -1) if no object is detected

--- assign1_ws/src/light_tracking/src/object_position.cpp
+++ assign1_ws/src/light_tracking/src/object_position.cpp
@@ -3,6 +3,7 @@
 #include "assign1_interfaces/msg/pixel_coordinates.hpp"
 #include <cv_bridge/cv_bridge.hpp>
 #include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
 
 using namespace std::chrono_literals;
      
@@ -58,14 +59,24 @@ private:
 
         threshold( grey, threshed, threshold_value_, 255, threshold_type );
 
-        // find moments of the image
-        cv::Moments m = cv::moments(threshed, true);
-        p_ = cv::Point(m.m10/m.m00, m.m01/m.m00);
-        
-        // show the image with a point mark at the centroid
-        circle(src, p_, 5, cv::Scalar(128,0,0), -1);
-        cv::imshow("Image with center",src);
-        cv::waitKey('q'); 
+        if(countNonZero(threshed))
+        {
+            // find moments of the image
+            cv::Moments m = cv::moments(threshed, true);
+            p_ = cv::Point(m.m10/m.m00, m.m01/m.m00);
+            
+            // show the image with a point mark at the centroid
+            circle(src, p_, 5, cv::Scalar(128,0,0), -1);
+            cv::imshow("Image with center", src);
+            cv::waitKey('q');
+        }
+        else
+        {
+            p_ = cv::Point(-1, -1);
+            RCLCPP_INFO(this->get_logger(), "no pixels bright enough");
+        }
+
+         
     }
 
     int threshold_value_ {};
